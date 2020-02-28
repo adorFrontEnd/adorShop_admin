33d9661397f36c277b88e07083a9a87076964555
masecho <masecho@163.com>
2020-02-28 10:25:20 +0800

    分类的分页取消

--- src/api/setting/ClasssifySetting.js
+++ src/api/setting/ClasssifySetting.js
@@ -6,8 +6,8 @@ import baseHttpProvider from '../base/baseHttpProvider';
 //     )
 // }
 const searchList = (params) => {
-    return baseHttpProvider.postFormApi('api/category/searchList', { page: 1, size: 10, ...params }, { total: true });
-  }
+    return baseHttpProvider.postFormApi('api/category/searchList', params, { total: true });
+}
 
 const saveOrUpdate = (params) => {
     return baseHttpProvider.postFormApi('api/category/saveOrUpdate', params)
@@ -21,7 +21,7 @@ const saveSort = (params) => {
 
 const deleteClassify = (params) => {
     return baseHttpProvider.getApi('api/category/delete', params)
-  }
+}
 export {
     searchList,
     saveOrUpdate,
