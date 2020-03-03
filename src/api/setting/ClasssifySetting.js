201fbe7e29ee6afeb18a9dae4937105c511cd4fc
liangjuan <1176541527@qq.com>
2020-03-03 11:41:52 +0800

    代码提交

---
+++ src/api/setting/ClasssifySetting.js
@@ -0,0 +1,32 @@
+import baseHttpProvider from '../base/baseHttpProvider';
+
+// const searchList = (params) => {
+
+//     return baseHttpProvider.postFormApi('api/category/searchList', null
+//     )
+// }
+const searchList = (params) => {
+    return baseHttpProvider.postFormApi('api/category/searchList', { page: 1, size: 10, ...params }, { total: true });
+    return baseHttpProvider.postFormApi('api/category/searchList', params, { total: true });
+}
+
+const saveOrUpdate = (params) => {
+    return baseHttpProvider.postFormApi('api/category/saveOrUpdate', params)
+}
+const levelList = (params) => {
+    return baseHttpProvider.getApi('api/category/levelList', params)
+}
+const saveSort = (params) => {
+    return baseHttpProvider.getApi('api/category/saveSort', params)
+}
+
+const deleteClassify = (params) => {
+    return baseHttpProvider.getApi('api/category/delete', params)
+}
+export {
+    searchList,
+    saveOrUpdate,
+    levelList,
+    saveSort,
+    deleteClassify
+}
